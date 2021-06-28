9c3f978c73db79d99d0af328af0b205169c42756
srinivas1246 <srinivasreddy.skim@gmail.com>
2021-06-28 11:37:57 +0530

    third commit

---
+++ src/main/resources/sql/bundles/maven-project1/port_data_production.sql
@@ -0,0 +1,5 @@
+INSERT INTO pagemodels (code, descr, frames, plugincode) VALUES ('maven-project1', 'webapp', '<frames>
+  <frame pos="0">
+    <descr>Sample I</descr>
+  </frame>
+</frames>', NULL);
